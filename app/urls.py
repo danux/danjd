0cd0404e93416be7132556725d425ba5435c4f5c
Daniel Davies <danieldavies127@gmail.com>
2015-11-17 20:12:42 +0000

    Green and 404 page

--- app/urls.py
+++ app/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # url(r'^$', 'danjd.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^404/$', TemplateView.as_view(template_name='404.html')),
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
